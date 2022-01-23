ed112afa185a96ee96011cda71b7b46f3857a8df
tkxksdl2 <tkxksdl2@gmail.com>
2022-01-23 13:21:47 +0900

    2022/01/23 합승 택시요금 kakao 2021 unefficient

--- 합승 택시 요금/solution.py
+++ 합승 택시 요금/solution.py
@@ -1,9 +1,5 @@
 def solution(n, s, a, b, fares):
-    root_a = []
-    root_b = []
 
-    node_used=[s]
-    fare_res =[]
     fares_dict = dict()
     for start, end, fare in fares:
         if start not in fares_dict:
@@ -15,30 +11,45 @@ def solution(n, s, a, b, fares):
         else:
             fares_dict[end][start] = fare
 
+    root_a = []
+    root_b = []
+
+    node_used=[s]
+    fare_res =[]
+
     dfs(n,s,a,b, fares_dict, node_used, fare_res, root_a, root_b)
 
-    print(root_a)
-    print(root_b)
+    answer = 100000 * (n-1)
+    
+    for a in root_a:
+        for b in root_b:
+            temp = 0
+            for i in range(len(a)):
+                if i <len(b) and a[i] == b[i]:
+                    continue
+                temp += a[i][2]
+            for i in range(len(b)):
+                temp += b[i][2]
 
-    return
+            if temp < answer:
+                answer = temp
+
+    return answer
 
 def dfs(n, s, a, b, fares_dict, node_used, fare_res, root_a, root_b):
     if s == a:
-        print(node_used)
-        root_a.append([node_used.copy(),fare_res.copy()])
+        root_a.append(fare_res.copy())
 
     if s == b:
-        print(node_used)
-        root_b.append([node_used.copy(), fare_res.copy()])
+        root_b.append(fare_res.copy())
 
     if len(node_used) == n:
         return
 
-
     for end, fare in fares_dict[s].items():
         if end not in node_used:
             node_used.append(end)
-            fare_res.append(fare)
+            fare_res.append([s, end, fare])
             dfs(n, end, a, b,  fares_dict, node_used, fare_res, root_a, root_b)
             node_used.pop()
             fare_res.pop()       
@@ -46,4 +57,4 @@ def dfs(n, s, a, b, fares_dict, node_used, fare_res, root_a, root_b):
 n, s, a, b = 6, 4, 6, 2
 fares = [[4, 1, 10], [3, 5, 24], [5, 6, 2], [3, 1, 41], [5, 1, 24], [4, 6, 50], [2, 4, 66], [2, 3, 22], [1, 6, 25]]
 
-solution(n,s,a,b,fares)
+print(solution(n,s,a,b,fares))
