8d46b1053142ffbb286aa121876b2bffcfe2c4a2
tkxksdl2 <tkxksdl2@gmail.com>
2022-01-26 15:10:28 +0900

    2022/01/26 무지의 먹방 라이브 efficiency failed

--- 무지의 먹방 라이브/solution.py
+++ 무지의 먹방 라이브/solution.py
@@ -1,3 +1,6 @@
+from tracemalloc import start
+
+
 def solution(food_times, k):
     if k > sum(food_times):
         return -1
@@ -6,7 +9,41 @@ def solution(food_times, k):
     for idx, time in enumerate(food_times):
         linked_dict[idx] = {'value':time,'pre':(idx-1)%len(food_times), 'nxt': (idx+1)%len(food_times)}
     
-    idx = 0
+    len_dict = len(linked_dict)
+    start_point = 0
+    while True:
+        min = 100000001
+        while linked_dict[start_point]['value'] == 0:
+            start_point = linked_dict[start_point]['nxt']
+
+        current = start_point
+        while True:
+            if linked_dict[current]['value'] < min: 
+                min = linked_dict[current]['value']
+            if linked_dict[current]['nxt'] < current:
+                break
+            current = linked_dict[current]['nxt']
+            
+
+        if min * len_dict < k:
+            k -= min * len_dict
+            current = start_point
+            while True:
+                linked_dict[current]['value'] -= min
+
+                if linked_dict[current]['value'] == 0:
+                    linked_dict[linked_dict[current]['pre']]['nxt'] = linked_dict[current]['nxt']
+                    linked_dict[linked_dict[current]['nxt']]['pre'] = linked_dict[current]['pre']
+                    len_dict -= 1
+                current = linked_dict[current]['nxt']
+                if linked_dict[current]['nxt'] < current:
+                    break
+
+        else: break
+            
+
+
+    idx = start_point
     while k > 0:
         linked_dict[idx]['value'] -= 1
         
@@ -18,3 +55,8 @@ def solution(food_times, k):
         k -=1
         
     return idx+1
+
+food_times = [3, 1, 2]
+k = 5
+
+print(solution(food_times, k))
