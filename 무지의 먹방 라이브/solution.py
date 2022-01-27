1e7122865ed56e42890328997efdfbe23957f8a0
tkxksdl2 <tkxksdl2@gmail.com>
2022-01-27 16:54:08 +0900

    2022/01/27 무지의먹방라이브 kakao 2019

--- 무지의 먹방 라이브/solution.py
+++ 무지의 먹방 라이브/solution.py
@@ -1,20 +1,35 @@
+
 def solution(food_times, k):
     if k >= sum(food_times):
         return -1
     
-    linked_dict = dict()
-    for idx, time in enumerate(food_times):
-        linked_dict[idx] = {'value':time,'pre':(idx-1)%len(food_times), 'nxt': (idx+1)%len(food_times)}
-    
-    idx = 0
-    while k > 0:
-        linked_dict[idx]['value'] -= 1
+    food_times = [[t,i] for i,t in enumerate(food_times)]
+    food_times.sort()
+
+    n_remain = len(food_times)
+    min = 0
+
+    for idx, v in enumerate(food_times):
+        t = v[0]
         
-        if linked_dict[idx]['value'] == 0:
-            linked_dict[linked_dict[idx]['pre']]['nxt'] = linked_dict[idx]['nxt']
-            linked_dict[linked_dict[idx]['nxt']]['pre'] = linked_dict[idx]['pre']
+        if t == min : 
+            n_remain -= 1
+            continue
+        elif t > min and (t-min) * n_remain <= k:
+            k -= (t-min) * n_remain
+            min = t
+            n_remain -= 1
         
-        idx = linked_dict[idx]['nxt']
-        k -=1
-        
-    return idx+1
+        elif k < (t-min) * n_remain:
+            k %= n_remain
+            res = food_times[idx:]
+            res.sort(key= lambda x: x[1])
+            return res[k][1] + 1
+
+
+
+
+food_times = [1,4,2,3,2]
+k = 9
+
+print(solution(food_times, k))
