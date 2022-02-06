2e0becf8cea67013c3634644f754e0573f49ecb3
tkxksdl2 <tkxksdl2@gmail.com>
2022-02-06 19:26:59 +0900

    2022/02/06 호텔 방 배정 kakao 2019

--- 호텔 방 배정/solution.py
+++ 호텔 방 배정/solution.py
@@ -2,36 +2,20 @@
 def solution(k, room_number):
     room_dict = dict()
     answer = []
+
     for n in room_number:
-        
-        if n in room_dict:
-            n = room_dict[n]['next'][0]
-
-        room_dict[n] = {'next' : [n+1, n], 'pre' : [n-1, n]}
+        temp = []
+        while n in room_dict:
+            temp.append(n)
+            n = room_dict[n]
+        room_dict[n] = n+1
+        for n_temp in temp:
+            room_dict[n_temp] = n+1
+            
         answer.append(n)
-        
-        if n+1 in room_dict and n-1 in room_dict:
-            print('양쪽')
-            room_dict[n]['next'] = room_dict[n+1]['next']
-            room_dict[n]['pre'] = room_dict[n-1]['pre']
-            room_dict[n-1]['next'] = room_dict[n+1]['next']
-            room_dict[n+1]['pre'] = room_dict[n-1]['pre']
-
-        elif n+1 in room_dict:
-            room_dict[n]['next'] = room_dict[n+1]['next']
-            room_dict[n]['pre'] = room_dict[n+1]['pre'] # 객체할당
-            room_dict[n]['pre'][0] = n-1
-
-        elif n-1 in room_dict:
-            room_dict[n]['pre'] = room_dict[n-1]['pre']
-            room_dict[n]['next'] = room_dict[n-1]['next']
-            room_dict[n]['next'][0] = n+1
-        print(room_dict)
-    
-
 
     return answer
 
 k = 10
-room_number = [3,1,5,4,2]
+room_number = [1,3,4,1,3,1]
 print(solution(k, room_number))
