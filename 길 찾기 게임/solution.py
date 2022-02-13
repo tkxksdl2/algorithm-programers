529c53069ec650cac7956797410d18a03361a81e
tkxksdl2 <tkxksdl2@gmail.com>
2022-02-13 15:14:05 +0900

    2022/02/13 길 찾기 게임 kakao 2019

--- 길 찾기 게임/solution.py
+++ 길 찾기 게임/solution.py
@@ -1,4 +1,7 @@
 from collections import deque
+import sys
+sys.setrecursionlimit(20000)
+
 def solution(nodeinfo):
     nodeinfo = [value +[i+1] for i, value in enumerate(nodeinfo)]
     nodeinfo.sort(key= lambda x: (-x[1], x[0]))
@@ -11,29 +14,30 @@ def solution(nodeinfo):
         if node[1] < y_min:
             depth_list.append([])
             y_min = node[1]
-        
-        tree[node[2]] = {'child':[], 'xy':(node[0],node[1])}
-        depth_list[-1].append(node[2])
-        
-        if len(depth_list) ==1: continue
 
-        min_distance = 100000 * 1000000
-        min_node = None
+        tree[node[2]] = {'child':[], 'xy':[node[0],node[1]]}
+        depth_list[-1].append(node[2])
+
+        if len(depth_list) ==1:
+            tree[node[2]]['bound'] = [-1, 100001]
+            continue
+
         for parents in depth_list[-2]:
-            distance = get_distance((node[0],node[1]), tree[parents]['xy'])
-            if distance < min_distance:
-                min_node = parents
-                min_distance = distance
-        tree[min_node]['child'].append(node[2])
+            if  tree[parents]['bound'][0] < node[0] < tree[parents]['xy'][0]:
+                tree[node[2]]['bound'] = [tree[parents]['bound'][0], tree[parents]['xy'][0]]
+                tree[parents]['child'].append(node[2])
+                break
+            elif tree[parents]['xy'][0] < node[0] < tree[parents]['bound'][1]:
+                tree[node[2]]['bound'] = [tree[parents]['xy'][0], tree[parents]['bound'][1]]
+                tree[parents]['child'].append(node[2])
+                break
+
     pre_list = []
     post_list = []
     order(tree, depth_list[0][0], pre_list)
     order(tree, depth_list[0][0], post_list, pre=False)
-    answer = [pre_list, post_list]
-    return answer
-
-def get_distance(p1,p2):
-    return (p1[0] - p2[0])**2 + (p1[1] - p2[1])**2
+     
+    return [pre_list, post_list]
 
 def order(tree, start, res, pre=True):
     if pre: res.append(start)
@@ -43,6 +47,7 @@ def order(tree, start, res, pre=True):
         else:   order(tree, q.popleft(), res, pre=False)
     if not pre: res.append(start)
     return
+
 
 nodeinfo = [[5,3],[11,5],[13,3],[3,5],[6,1],[1,3],[8,6],[7,2],[2,2]]
 print(solution(nodeinfo))
