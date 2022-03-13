6d831a8b86915cc0d8ffd80dc37d593e75c2e924
tkxksdl2 <tkxksdl2@gmail.com>
2022-03-13 19:37:35 +0900

    2022/03/13 영화감독 숀 backjoon brute

--- [backjoon]/brute/영화감독 숌/solution.py
+++ [backjoon]/brute/영화감독 숌/solution.py
@@ -1,10 +1,9 @@
-n = int(input())
 def solution(n):
-    num = 666
-    cnt = 1
+    num, cnt = 666, 1
+
     while cnt < n:
         num +=1
         if '666' in str(num):
             cnt += 1
     return num
-print(solution(n))
+print(solution(int(input())))
