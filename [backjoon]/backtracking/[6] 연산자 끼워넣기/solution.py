e19346198ca028fc22f6051b8c0d55be082fd946
tkxksdl2 <tkxksdl2@gmail.com>
2022-04-06 05:54:29 +0900

    2022/04/06 연산자 끼워넣기 backtracking

---
+++ [backjoon]/backtracking/[6] 연산자 끼워넣기/solution.py
@@ -0,0 +1,47 @@
+def solution(n, nums, cals):
+    global maxval, minval
+    
+    maxmin = [-(10**9) -1, 10**9 + 1]
+    
+    
+    
+
+    def dfs(step, preval, maxmin):
+        if step == n:
+            if preval > maxmin[0]: maxmin[0] = preval
+            if preval < maxmin[1]: maxmin[1] = preval
+            return
+
+        print(step, preval)
+        for c in range(4):
+            if cals[c]:
+                cals[c] -= 1
+
+                if c == 0:
+                    dfs(step+1, preval + nums[step], maxmin)
+                elif c == 1:
+                    dfs(step+1, preval - nums[step], maxmin)
+                elif c == 2:
+                    dfs(step+1, preval * nums[step], maxmin)
+                else:
+                    if preval < 0 and nums[step] > 0:
+                        dfs(step+1, -( -preval // nums[step]), maxmin)
+                    else:
+                        dfs(step+1, preval // nums[step], maxmin)
+                
+                cals[c] += 1
+                    
+        return
+
+    dfs(1, nums[0], maxmin)
+
+    print(maxmin)
+
+    return
+
+
+
+n = int(input())
+nums = list(map(int, input().split())) 
+cals = list(map(int, input().split())) 
+solution(n, nums, cals)
