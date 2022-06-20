e53676b778bf4f6523569bc2eb360b375f5e0f16
tkxksdl2 <tkxksdl2@gmail.com>
2022-06-21 04:55:12 +0900

    2022/06/21 스택 stack

---
+++ [backjoon]/stack/[1] 스택/solution.py
@@ -0,0 +1,43 @@
+from collections import deque
+
+class Stack():
+    def __init__(self):
+        self.arr = deque()
+        self.len = 0
+
+    def proc(self, command=list()):
+        w = command[0]
+        if w == 'push':
+            self.push(int(command[1]))
+        elif w == 'pop':
+            if      self.isEmpty(): print(-1)
+            else:   self.pop()
+        elif w == 'top':
+            if      self.isEmpty(): print(-1)
+            else:   print(self.arr[0])
+        elif w == 'empty':
+            print(self.isEmpty())
+        elif w == 'size':
+            print(self.len)
+
+    def push(self, num):
+        self.arr.append(num)
+        self.len += 1 
+    
+    def pop(self):
+        print(self.arr.pop())
+        self.len -= 1
+
+    def isEmpty(self):
+        if self.len == 0:
+            return  1
+        else: return 0
+n = int(input())
+Stk = Stack()
+
+for _ in range(n):
+    command = input().split()
+    Stk.proc(command)
+
+
+        
